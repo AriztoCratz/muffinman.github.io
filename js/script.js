519d3b037a6df3408721e07609dea60c662b3afc
AriztoCratz <jehu.baldago@gmail.com>
2023-03-31 16:12:07 +0800

    changes 2

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ window.onscroll = () =>{
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
-}
+};
+
+//dark light mode
+
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onlick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
