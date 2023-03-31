71061f06d9d9e2590c27d82188fcdaaaa8f5b920
AriztoCratz <jehu.baldago@gmail.com>
2023-03-31 23:45:29 +0800

    changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
-//sticky nav bar
-window.onscroll = () =>{
+//menu icon navbar
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
 
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+//scroll section active links
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
+
+//remove link 
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
+
+
 
 //dark light mode
 
 let darkModeIcon = document.querySelector('#darkMode-icon');
 
-darkModeIcon.onlick = () => {
+
+darkModeIcon.onclick = () =>{
     darkModeIcon.classList.toggle('bx-sun');
-    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('dark-mode')
 };
